be2a97c1cad7e34af45c0c8d1143ada81d7f2e89
FedericoPessina02 <s293945@studenti.polito.it>
2024-05-21 01:48:40 +0200

    Piccole modifiche

--- Project/main.cpp
+++ Project/main.cpp
@@ -7,9 +7,10 @@ using namespace std;
 
 int main()
 {
+    int partition_dimension = 2;
     array<double, 6> domain_borders;
     vector<Fracture> fractures = Utils::fractureInput("./DFN/FR3_data.txt", domain_borders); //elenco fratture
-    map<int, vector<Fracture>> id_to_fractures = Algorithms::assignPartition(fractures, domain_borders, 2);
+    map<int, vector<Fracture>> id_to_fractures = Algorithms::assignPartition(fractures, domain_borders, partition_dimension);
     TracesMesh mesh;
     cout << id_to_fractures.size();
     Algorithms::cutTraces(id_to_fractures, mesh);
